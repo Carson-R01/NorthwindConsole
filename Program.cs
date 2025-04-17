d68c85d04492869548a21b88f9432df4dada3bf1
carsonreiter <creiter3@my.wctc.edu>
2025-04-17 13:18:45 -0500

    [1.10] Display categories

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using NLog;
 using System.Linq;
  using NorthwindConsole.Model;
+ using Microsoft.Extensions.Configuration;
+ using Microsoft.EntityFrameworkCore;
 
  string path = Directory.GetCurrentDirectory() + "//nlog.config";
  
@@ -21,6 +23,23 @@ do
    if (choice == "1")
    {
      // display categories
+
+     var configuration = new ConfigurationBuilder()
+             .AddJsonFile($"appsettings.json");
+ 
+     var config = configuration.Build();
+ 
+     var db = new DataContext();
+     var query = db.Categories.OrderBy(p => p.CategoryName);
+ 
+     Console.ForegroundColor = ConsoleColor.Green;
+     Console.WriteLine($"{query.Count()} records returned");
+     Console.ForegroundColor = ConsoleColor.Magenta;
+     foreach (var item in query)
+     {
+       Console.WriteLine($"{item.CategoryName} - {item.Description}");
+     }
+     Console.ForegroundColor = ConsoleColor.White;
    }
    else if (choice == "2")
    {
