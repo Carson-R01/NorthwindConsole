7613393f8b864d6e82202db55095d05037d5c489
carsonreiter <creiter3@my.wctc.edu>
2025-04-17 13:17:58 -0500

    [1.09] Create user options

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using NLog;
+using System.Linq;
+ using NorthwindConsole.Model;
+
  string path = Directory.GetCurrentDirectory() + "//nlog.config";
  
  // create instance of Logger
@@ -6,6 +9,28 @@
  
  logger.Info("Program started");
  
- Console.WriteLine("Hello World!");
+do
+ {
+   Console.WriteLine("1) Display categories");
+   Console.WriteLine("2) Add category");
+   Console.WriteLine("Enter to quit");
+   string? choice = Console.ReadLine();
+   Console.Clear();
+   logger.Info("Option {choice} selected", choice);
+ 
+   if (choice == "1")
+   {
+     // display categories
+   }
+   else if (choice == "2")
+   {
+     // Add category
+   }
+   else if (String.IsNullOrEmpty(choice))
+   {
+     break;
+   }
+   Console.WriteLine();
+ } while (true);
  
  logger.Info("Program ended");
